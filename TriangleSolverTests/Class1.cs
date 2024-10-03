debfd33cd7a9f10a23ce3451395f4c9ebfef3bd9
Abhilash <Agangadasu1502@conestogac.on.ca>
2024-10-02 22:47:31 -0400

    Completed Equilateral and Isosceles triangle tests

---
+++ TriangleSolverTests/Class1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using TriangleSolver;
+
+namespace TriangleSolverTests
+{
+    public class Class1
+    {
+
+[TestFixture]
+    public class TriangleSolverTests
+    {
+        [Test]
+        public void AnalyzeTriangle_ValidEquilateralTriangle_ReturnsEquilateralTriangle()
+        {
+            // Arrange
+            int side1 = 5;
+            int side2 = 5;
+            int side3 = 5;
+            string expected = "Equilateral triangle";
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            // Assert
+            ClassicAssert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void AnalyzeTriangle_ValidIsoscelesTriangle_FirstTwoSidesEqual_ReturnsIsoscelesTriangle()
+        {
+            // Arrange
+            int side1 = 5;
+            int side2 = 5;
+            int side3 = 7;
+            string expected = "Isosceles triangle";
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            // Assert
+            ClassicAssert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void AnalyzeTriangle_ValidIsoscelesTriangle_LastTwoSidesEqual_ReturnsIsoscelesTriangle()
+        {
+            // Arrange
+            int side1 = 7;
+            int side2 = 5;
+            int side3 = 5;
+            string expected = "Isosceles triangle";
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            // Assert
+            ClassicAssert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void AnalyzeTriangle_ValidIsoscelesTriangle_FirstAndLastSidesEqual_ReturnsIsoscelesTriangle()
+        {
+            // Arrange
+            int side1 = 5;
+            int side2 = 7;
+            int side3 = 5;
+            string expected = "Isosceles triangle";
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            // Assert
+            ClassicAssert.AreEqual(expected, result);
+        }
+
+        
+    }
+}
+}
